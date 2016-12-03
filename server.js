b78330f75ccec38426b591769690abeb973b9c81
akotamarthi <akotamarthi@ccs.neu.edu>
2016-12-02 21:42:01 -0500

    assignment 5 part 3

--- server.js
+++ server.js
@@ -5,20 +5,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
-
-
 var connectionString ='mongodb://alekhya.jntu@gmail.com:Portal20@ds033337.mongolab.com:33337/webdev';
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
 
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
 
-//require ("./test/app.js")(app);
-require ("./assignment/app.js")(app);
-
-
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-app.listen(port, ipaddress);
+var assignment=require("./assignment/app");
+assignment(app);
+app.listen(4000);
