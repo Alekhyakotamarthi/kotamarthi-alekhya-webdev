56629067b0301c9699032b454e718020f9ac7aeb
akotamarthi <akotamarthi@ccs.neu.edu>
2016-12-02 21:45:10 -0500

    assignment 5 part 3

--- test/app.js
+++ test/app.js
@@ -6,13 +6,14 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    /*if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
             process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
             process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
             process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
             process.env.OPENSHIFT_APP_NAME;
     }
+    */
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
